c71cb9d690296a47b018f2d8a1abdea6e6723029
pamela <1620137@cegepoutaouais.qc.ca>
2021-11-26 14:03:12 -0500

    Test push sans gitignore

---
+++ build/web/WEB-INF/Views/error.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : error
+    Created on : 26 nov. 2021, 14 h 00 min 28 s
+    Author     : pamga
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
--- src/java/Controllers/HomeController.java
+++ src/java/Controllers/HomeController.java
@@ -1,15 +1,22 @@
 package Controllers;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
- * @author pamga
+ * @author Pamela Gariepy
  */
+@Controller
 public class HomeController {
-    
+
+     @RequestMapping(value="/Home/accueil", method=RequestMethod.GET) 
+    public ModelAndView accueil(){
+        // Création de l'objet ModelAndView
+        ModelAndView modelAndView = new ModelAndView("accueil");
+        return modelAndView;  
+
+    }   
 }
